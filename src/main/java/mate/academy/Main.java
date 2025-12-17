d476140c3516edfc8342614e58698a441b6cfbc7
mr-Edoc <mr.edoc.work@gmail.com>
2025-12-17 02:29:03 +0200

    Ver1/Connect and config Hibernate in this project, Developed Movie model, also MovieDao, MovieService and implementations for it.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,24 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie();
+        movie.setTitle("Movie_Name");
+        movie.setDescription("Description");
+
+        Movie addedMovie = movieService.add(movie);
+        System.out.println("Added movie: " + addedMovie);
+
+        Movie movieFromDB = movieService.get(movie.getId());
+        System.out.println("Movie by id - " + movie.getId() + ": " + movieFromDB);
 
     }
 }
